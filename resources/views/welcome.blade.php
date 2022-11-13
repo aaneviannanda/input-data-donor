5f23623c640254fb528c8d3d3f1030c4133e1327
Aan Evian Nanda <71364724+aaneviannanda@users.noreply.github.com>
2022-11-13 22:32:55 +0700

    update animation ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,8 +38,12 @@
         <!-- Sweet Alert -->
         <script src="{{ asset('js/sweetalert.min.js') }}"></script>  
         
-        <!-- Boostrap Icon -->
-        <link rel="stylesheet" href="{{ asset('bootstrap-icons/bootstrap-icons.css') }}" />
+        <!-- vendor CSS -->
+        <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/vendor/aos/aos.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/vendor/glightbox/css/glightbox.min.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/vendor/remixicon/remixicon.css') }}" />
+        <link rel="stylesheet" href="{{ asset('assets/vendor/swiper/swiper-bundle.min.css') }}" />
     </head>
     <body class="antialiased">
         {{-- @if (Route::has('login'))
@@ -92,14 +96,14 @@
             <div class="container">
             <div class="row">
                 <div class="col-lg-6 d-flex flex-column justify-content-center">
-                <h1 data-aos="fade-up">We offer modern solutions for growing your business</h1>
-                <h2 data-aos="fade-up" data-aos-delay="400">We are team of talented designers making websites with Bootstrap</h2>
+                <h1 data-aos="fade-up">Development of Information System for Donor Data Input</h1>
+                <h2 data-aos="fade-up" data-aos-delay="400">Donate your blood for humanity. Call the hotline: <span>031 5313289</span></h2>
                 <div data-aos="fade-up" data-aos-delay="600">
                     <div class="text-center text-lg-start">
-                    <a href="#about" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
-                        <span>Get Started</span>
-                        <i class="bi bi-arrow-right"></i>
-                    </a>
+                        <a href="{{ route('login') }}" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
+                            <span>Get Started</span>
+                            <i class="bi bi-arrow-right"></i>
+                        </a>
                     </div>
                 </div>
                 </div>
@@ -119,9 +123,13 @@
                 <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
                 <div class="content">
                     <h3>ABOUT IDP</h3>
-                    <h2>Expedita voluptas omnis cupiditate totam eveniet nobis sint iste. Dolores est repellat corrupti reprehenderit.</h2>
+                    <h2>The Surabaya City PMI Blood Transfusion Unit (UTD) under the management of the Surabaya City PMI. 
+                        Located at Jalan Embong Ploso number 7-15 Surabaya, under the leadership of Dr. Hj Budi Arifah, 
+                        serves the people of Surabaya, especially East Java in general and other places besides East Java Province.</h2>
                     <p>
-                    Quisquam vel ut sint cum eos hic dolores aperiam. Sed deserunt et. Inventore et et dolor consequatur itaque ut voluptate sed et. Magnam nam ipsum tenetur suscipit voluptatum nam et est corrupti.
+                        In 1955 PMI was located at Jl.Embong Ploso 7-9 and Dr. Payma was replaced by Dr. Suharyono. Dr. Payma passed away in the Netherlands while studying. 
+                        In 1972, Dr. Suharyono was replaced by Dr. Alfian and after the expansion of the building, it was named: Blood Transfusion Service. 
+                        With this expansion: blood donation activities were placed on Jl.Embong Ploso 7-9 and for laboratory space on Jl.Embong Ploso 11-15.
                     </p>
                     <div class="text-center text-lg-start">
                     <a href="#" class="btn-read-more d-inline-flex align-items-center justify-content-center align-self-center">
@@ -155,24 +163,24 @@
                     <div class="col-lg-4" data-aos="fade-up" data-aos-delay="200">
                         <div class="box">
                             <img src="{{ asset('img/aan.png') }}" class="img-fluid img__team" alt="">
-                            <h3>Ad cupiditate sed est odio</h3>
-                            <p>Eum ad dolor et. Autem aut fugiat debitis voluptatem consequuntur sit. Et veritatis id.</p>
+                            <h3>Aan Evian Nanda</h3>
+                            <p>7th semester student majoring in informatics of the East Java Veterans National Development University</p>
                         </div>
                     </div>
         
                     <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="400">
                         <div class="box">
                             <img src="{{ asset('img/alfin.png') }}" class="img-fluid img__team" alt="">
-                            <h3>Voluptatem voluptatum alias</h3>
-                            <p>Repudiandae amet nihil natus in distinctio suscipit id. Doloremque ducimus ea sit non.</p>
+                            <h3>Alfinas Agung Mujiono</h3>
+                            <p>7th semester student majoring in informatics of the East Java Veterans National Development University</p>
                         </div>
                     </div>
         
                     <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="600">
                         <div class="box">
                             <img src="{{ asset('img/nico.png') }}" class="img-fluid img__team" alt="">
-                            <h3>Fugit cupiditate alias nobis.</h3>
-                            <p>Quam rem vitae est autem molestias explicabo debitis sint. Vero aliquid quidem commodi.</p>
+                            <h3>Nico Natanael</h3>
+                            <p>7th semester student majoring in informatics of the East Java Veterans National Development University</p>
                         </div>
                     </div>
         
@@ -187,10 +195,12 @@
                     <div class="row gy-4">
                         <div class="col-lg-5 col-md-12 footer-info">
                             <a href="index.html" class="logo d-flex align-items-center">
-                            <img src="assets/img/logo.png" alt="">
+                            <img src="" alt="">
                             <span>IDP</span>
                             </a>
-                            <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
+                            <p>
+                                The Blood Donor Data Entry Information System at the Surabaya City Indonesian Red Cross Blood Transfusion Unit is a system used to manage donors' personal data more effectively and efficiently.
+                            </p>
                             <div class="social-links mt-3">
                                 <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
                                 <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
@@ -203,32 +213,26 @@
                             <h4>Useful Links</h4>
                             <ul>
                             <li><i class="bi bi-chevron-right"></i> <a href="#">Home</a></li>
-                            <li><i class="bi bi-chevron-right"></i> <a href="#">About us</a></li>
-                            <li><i class="bi bi-chevron-right"></i> <a href="#">Services</a></li>
-                            <li><i class="bi bi-chevron-right"></i> <a href="#">Terms of service</a></li>
-                            <li><i class="bi bi-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                            <li><i class="bi bi-chevron-right"></i> <a href="#about">About us</a></li>
+                            <li><i class="bi bi-chevron-right"></i> <a href="#footer">Contacts</a></li>
                             </ul>
                         </div>
 
                         <div class="col-lg-2 col-6 footer-links">
                             <h4>Our Services</h4>
                             <ul>
-                            <li><i class="bi bi-chevron-right"></i> <a href="#">Web Design</a></li>
-                            <li><i class="bi bi-chevron-right"></i> <a href="#">Web Development</a></li>
-                            <li><i class="bi bi-chevron-right"></i> <a href="#">Product Management</a></li>
-                            <li><i class="bi bi-chevron-right"></i> <a href="#">Marketing</a></li>
-                            <li><i class="bi bi-chevron-right"></i> <a href="#">Graphic Design</a></li>
+                            <li><i class="bi bi-chevron-right"></i> <a target="_blank" href="http://www.sby.uddpmikotasby.com/">Web Profile</a></li>
                             </ul>
                         </div>
 
                         <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
                             <h4>Contact Us</h4>
                             <p>
-                            A108 Adam Street <br>
-                            New York, NY 535022<br>
-                            United States <br><br>
-                            <strong>Phone:</strong> +1 5589 55488 55<br>
-                            <strong>Email:</strong> info@example.com<br>
+                                AXA Insurance <br>
+                            Jl. Sumatera No. 16 Surabaya<br>
+                            Indonesia <br><br>
+                            <strong>Phone:</strong> (+62) 31 5313289<br>
+                            <strong>FAQ:</strong> (+62) 31 53<br>
                             </p>
 
                         </div>
@@ -238,9 +242,23 @@
             </div>
             <div class="container">
                 <div class="copyright">
-                    &copy; Copyright <strong><span>IDP</span></strong>. 2022
+                    &copy; Copyright <strong><span>IDP</span></strong> 2022
                 </div>
             </div>
         </footer><!-- End Footer -->
-    </body>
+        <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+        <!-- Template Main JS File -->
+
+        <!-- Vendor JS Files -->
+        <script src="{{ asset('assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
+        <script src="{{ asset('assets/vendor/aos/aos.js') }}"></script>
+        <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+        <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
+
+        <!-- Template Main JS File -->
+        <script src="{{ asset('assets/js/main.js') }}"></script>
+        </body>
 </html>
